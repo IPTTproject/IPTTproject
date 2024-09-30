2505d9bcd53ce642f38ccf0b58df0dd5affb7d33
JackyTheWise <ijackyzhuang@gmail.com>
2024-09-30 14:19:41 +0800

    Update dataML.py

--- app_config/dataML.py
+++ app_config/dataML.py
@@ -2,7 +2,6 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder as OHE
+from sklearn.preprocessing import LabelEncoder
 df = pd.read_csv("data1.csv")
-df["房地總價"] = df["房地總價"].replace(",", "", regex = True).astype(float)
-print(df.dtypes)
-
