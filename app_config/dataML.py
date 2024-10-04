85a922af6dd96dbea6cd3195b0bc17864a3758ea
JackyTheWise <ijackyzhuang@gmail.com>
2024-10-04 20:38:44 +0800

    Update dataML2.py

--- app_config/dataML.py
+++ app_config/dataML.py
@@ -2,8 +2,17 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import OneHotEncoder as OHE
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
 df = pd.read_csv("data.csv")
+#將交易日期的值改成年份
+df["交易日期"] = df["交易日期"]//10000
+df = df.rename(columns = {"交易日期": "交易年份"})
+#將房價數值轉為整數
 df["房地總價"] = df["房地總價"].replace(",", "", regex = True).astype(int)
+#將房屋層次分為低、中、高樓層
 df["代表建號層次"] = df["代表建號層次"].str.split(";", expand = True)[0]
 df.loc[df["代表建號層次"].isin(["0", "見其他登記事項", "地下一層", "地下層", "騎樓", "地下三層", "地下二層"]), "代表建號層次"] = "其它"
 df["代表建號層次"] = df["代表建號層次"].apply(lambda x: "其它" if x in "其它" 
@@ -13,17 +22,24 @@ df["代表建號層次"] = df["代表建號層次"].apply(lambda x: "其它" if 
                                                       else "高樓層")
 floor_dict = {"其它": 0, "低樓層": 1, "中樓層": 2, "高樓層": 3}
 df["樓層_Lebel"] = df["代表建號層次"].map(floor_dict)
-df = pd.get_dummies(df, columns = ["行政區", "交易標的種類", "地段"])
-df = df.select_dtypes(exclude = "object")
+one = OHE(sparse_output = False)
+onehot = one.fit_transform(df[["行政區"]])
+行政區_df = pd.DataFrame(onehot, columns = one.get_feature_names_out(["行政區"])).astype(int)
+type_dict = {"建物": 0, "房地(土地+建物)": 1, "房地(土地+建物)+車位": 2}
+df["交易種類_Lebel"] = df["交易標的種類"].map(type_dict)
+df = df.drop(["交易標的種類", "代表建號層次", "行政區"], axis = 1)
 df = df.astype(int)
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
-y = df.房地總價
-X = df.drop("房地總價", axis = 1)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 42)
-model = RandomForestRegressor(n_estimators = 100, random_state = 42)
+final_df = pd.concat([df, 行政區_df], axis = 1)
+X = final_df.drop(["房地總價"], axis = 1)
+y = final_df["房地總價"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
 y_pred = model.predict(X_test)
-score = r2_score(y_test, y_pred)
-print("準確度為：", score, sep = "")
+mse = mean_squared_error(y_test, y_pred)
+print(f"MSE: {mse}")
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_pred)
+print("r2:", r2)
