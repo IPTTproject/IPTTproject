0a70d5513cc2ff4728ec219a976ad912512e916c
JackyTheWise <ijackyzhuang@gmail.com>
2024-10-05 13:11:52 +0800

    更正機器學習py擋
    移除離群值，提升分數。

--- app_config/dataML2.py
+++ app_config/dataML2.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder as OHE
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, accuracy_score
 df = pd.read_csv("data.csv")
 #將交易日期的值改成年份
 df["交易日期"] = df["交易日期"]//10000
@@ -30,5 +30,29 @@ df["交易種類_Lebel"] = df["交易標的種類"].map(type_dict)
 df = df.drop(["交易標的種類", "代表建號層次", "行政區"], axis = 1)
 df = df.astype(int)
 final_df = pd.concat([df, 行政區_df], axis = 1)
-for row in final_df:
-    print(row)
+for i in ["幾房", "建物移轉面積", "土地移轉面積", "幾廳", "幾衛", "車位數量", "房地總價"]:
+    s = final_df[i].describe()
+    IQR = s["75%"] - s["25%"]
+    upper_lim = s["75%"] + (IQR * 1.5)
+    lower_lim = s["25%"] - (IQR * 1.5)
+    final_df = final_df[final_df[i] < upper_lim]
+#for col in final_df:
+#    print(final_df[col].describe())
+#    print("-" * 50)
+X = final_df.drop(["房地總價"], axis = 1)
+y = final_df["房地總價"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = RandomForestRegressor(n_estimators=100, random_state=42)
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+print(f"MSE: {mse}")
+rmse = np.sqrt(mse)
+print("RMSE:", rmse)
+from sklearn.metrics import r2_score
+r2 = r2_score(y_test, y_pred)
+print("r2:", r2)
+from sklearn.model_selection import cross_val_score
+rfModel_cv = RandomForestRegressor(n_estimators = 100, random_state = 42)
+scores = cross_val_score(rfModel_cv, X, y, cv = 5, scoring = "r2")
+print("隨機森林回歸 r2 分數: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()*2))
