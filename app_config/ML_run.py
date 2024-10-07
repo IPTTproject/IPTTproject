312ec09158c2fb2b7ab03b3ebc25fd9a63bbf8cd
JackyTheWise <ijackyzhuang@gmail.com>
2024-10-07 09:58:24 +0800

    優化
    移除不必要的 index.html
    更新 ML_rum.py，移除不必要的變數

--- app_config/ML_run.py
+++ app_config/ML_run.py
@@ -32,87 +32,8 @@ def ML():
         trade_type = 1
     else:
         trade_type = 2
-    district = request.form["district"]
-    match district:
-        case "Qianjin":
-            Qianjin = 1
-        case "Xinxing":
-            Xinxing = 1
-        case "Yancheng":
-            Yancheng = 1
-        case "Zuoying":
-            Zuoying = 1
-        case "Nanzih":
-            Nanzih = 1
-        case "Gushan":
-            Gushan = 1
-        case "Qijin":
-            Qijin = 1
-        case "Lingya":
-            Lingya = 1
-        case "Sanmin":
-            Sanmin = 1
-        case "Qianzhen":
-            Qianzhen = 1
-        case "Xiaogang":
-            Xiaogang = 1
-        case "Fengshan":
-            Fengshan = 1
-        case "Niaosong":
-            Niaosong = 1
-        case "Dashe":
-            Dashe = 1
-        case "Renwu":
-            Renwu = 1
-        case "Dasu":
-            Dasu = 1
-        case "Gangshan":
-            Gangshan = 1
-        case "Yanshao":
-            Yanshao = 1
-        case "Zihguan":
-            Zihguan = 1
-        case "Yongan":
-            Yongan = 1
-        case "Mitou":
-            Mitou = 1
-        case "Qiaotou":
-            Qiaotou = 1
-        case "Tianliao":
-            Tianliao = 1
-        case "Qieding":
-            Qieding = 1
-        case "Alian":
-            Alian = 1
-        case "Luzhu":
-            Luzhu = 1
-        case "Hunei":
-            Hunei = 1
-        case "Namaxia":
-            Namaxia = 1
-        case "Taoyuan":
-            Taoyuan = 1
-        case "Maolin":
-            Maolin = 1
-        case "Liugui":
-            Liugui = 1
-        case "Meinong":
-            Meinong = 1
-        case "Qishan":
-            Qishan = 1
-        case "Jiaxian":
-            Jiaxian = 1
-        case "Neimen":
-            Neimen = 1
-        case "Sanlin":
-            Sanlin = 1
-        case "Linyuan":
-            Linyuan = 1
-        case "Daliao":
-            Daliao = 1
-            
-
-    input = np.array(
+    district = int(request.form["district"])
+    input_array = np.array(
         [113, #交易年份
         age, #代表建號屋齡
         rooms, #幾房
@@ -123,48 +44,15 @@ def ML():
         building_area,#建物移轉面積(平方公尺)
         parking_spots_area, #車位總持分面積(平方公尺)
         floor,  #樓層_Lebel  {"其它": 0, "低樓層": 1, "中樓層": 2, "高樓層": 3}
-        trade_type,  #交易種類_Lebel {"建物": 0, "房地(土地+建物)": 1, "房地(土地+建物)+車位": 2}
-        Sanmin,  #三名區
-        Renwu,  #仁武區
-        Neimen,  #內門區
-        Liugui,  #六龜區
-        Qianjin,  #前金區
-        Qianzhen,  #前鎮區
-        Daliao,  #大寮區
-        Dasu,  #大樹區
-        Dashe,  #大社區
-        Xiaogang,  #小港區
-        Gangshan,  #岡山區
-        Zuoying,  #左營區
-        Mitou,  #彌陀區
-        Xinxing,  #新興區
-        Qishan,  #旗山區
-        Qijin,  #旗津區
-        Sanlin,  #杉林區
-        Linyuan,  #林園區
-        Taoyuan,  #桃源區
-        Zihguan,  #梓官區
-        Nanzih,  #楠梓區
-        Qiaotou,  #橋頭區
-        Yongan,  #永安區
-        Hunei,  #湖內區
-        Yanshao,  #燕巢區
-        Tianliao,  #田寮區
-        Jiaxian,  #甲仙區
-        Meinong,  #美濃區
-        Lingya,  #苓雅區
-        Maolin,  #茂林區
-        Qieding,  #茄萣區
-        Luzhu,  #蘆竹區
-        Namaxia,  #那瑪夏區
-        Alian,  #阿蓮區
-        Niaosong,  #鳥松區
-        Fengshan,  #鳳山區
-        Yancheng,  #鹽埕區
-        Gushan,  #鼓山區
-        ]).reshape(1,49)
+        trade_type])  #交易種類_Lebel {"建物": 0, "房地(土地+建物)": 1, "房地(土地+建物)+車位": 2}
+    for i in range(district):
+        input_array = np.append(input_array, 0)
+    input_array = np.append(input_array, 1)
+    for i in range(37 - district):
+        input_array = np.append(input_array, 0)
+    input_array = input_array.reshape(1, 49)
     model = load("MLmodel.joblib")
-    price_pred = int(model.predict(input)[0]//10000)
+    price_pred = int(model.predict(input_array)[0]//10000)
     return f"<h1>我們的模型預測您的房價為{price_pred}萬元。</h1>"
 if __name__ == "__main__":
     app.run()
